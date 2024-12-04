561bc2ae4a111eca76a214cd29132a643ef12e0a
Ajay Tripathi <ajay39in@gmail.com>
2024-12-04 20:49:26 +0530

    feat(rsbuild): Add SSR capabilities

--- src/index.client.tsx
+++ src/index.client.tsx
@@ -9,8 +9,8 @@ if (!rootElement) {
   throw new Error("Failed to find the root element");
 }
 
-const root = ReactDOM.createRoot(rootElement);
-root.render(
+ReactDOM.hydrateRoot(
+  rootElement,
   <React.StrictMode>
     <AppRouter />
   </React.StrictMode>
